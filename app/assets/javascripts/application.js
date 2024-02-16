5349d233e3b4774e9c06700b5c3af6faebcf9629
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-16 13:05:21 +0800

    Add css style to index.html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,5 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+import "tailwindcss/tailwind.css";
